b457eb2a98c672857076b118eaf6928df6a1c65e
Robbie Selwyn <robbieselwyn@gmail.com>
2021-12-08 02:55:03 -0800

    Build error fix

--- cpp/astar_node.cpp
+++ cpp/astar_node.cpp
@@ -13,8 +13,8 @@
 
 std::pair<int,int> snap_to_grid(std::pair<double, double> x, int resolution) {
 	std::pair<int, int> newX;
-	newX[0] = resolution * round(x[0] / resolution);
-	newX[1] = resolution * round(x[1] / resolution);
+	newX.first = resolution * round(x.first / resolution);
+	newX.second = resolution * round(x.second / resolution);
 	return newX;
 }
 
@@ -23,13 +23,13 @@ bool isFree(std::pair<int, int> state, double window_size, double resolution, in
 	int lower = -1 * (int) round((window_size - 1) / 2);
 	int upper = (int) round((window_size - 1) / 2);
 	for (int dx = lower; dx < upper + 1; dx++) {
-		for (int dy = lower, dy < upper + 1; dy++) {
-			std::pair<double, double> old_xy = {state[0] + resolution * dx, state[1] + resolution * dy};
+		for (int dy = lower; dy < upper + 1; dy++) {
+			std::pair<double, double> old_xy = {state.first + resolution * dx, state.second + resolution * dy};
 			std::pair<int, int> xy = snap_to_grid(old_xy);
-			int grid_x = (int) ((x - origin_x) / resolution);
-			int grid_y = (int) ((y - origin_y) / resolution);
+			int grid_x = (int) ((xy.first - origin_x) / resolution);
+			int grid_y = (int) ((xy.second - origin_y) / resolution);
 
-			if (xy[0] > 0 && xy[1] > 0 && xy[0] < width && xy[1] < height) {
+			if (xy.first > 0 && xy.second > 0 && xy.first < width && xy.second < height) {
 				p_total *= (1.0 - max(0, (float) probs[grid_y * width + grid_x] / 100))
 			}
 		}
@@ -37,25 +37,25 @@ bool isFree(std::pair<int, int> state, double window_size, double resolution, in
 	return (1 - p_total) < 0.5;
 
 struct AStarAlgorithmParams {
-	set<std::pair<int, int>> closed_set;
-	set<std::pair<int, int>> open_set;
+	std::set<std::pair<int, int>> closed_set;
+	std::set<std::pair<int, int>> open_set;
 
-	map<std::pair<int, int>, int> est_cost_through;
-	map<std::pair<int, int>, int> cost_to_arrive;
-	map<std::pair<int, int>, std::pair<int, int>> came_from;
+	std::map<std::pair<int, int>, int> est_cost_through;
+	std::map<std::pair<int, int>, int> cost_to_arrive;
+	std::map<std::pair<int, int>, std::pair<int, int>> came_from;
 
-	vector<std::pair<int, int>> path;
+	std::vector<std::pair<int, int>> path;
 }
 
 bool is_state_free(x, AStar::Request &input_params) {
-	return isFree(x) && x[0] > 0 && x[1] > 0 && x[0] < input_params.width && x[1] < input_params.height;
+	return isFree(x) && x.first > 0 && x.second > 0 && x.first < input_params.width && x.second < input_params.height;
 }
 
 std::vector<std::pair<int, int>> get_neighbors(AStar::Request input, int r, std::pair<int, int> xy) {
 	std::vector<std::pair<int, int>> offsets = {{r, 0}, {-r, 0}, {0, r}, {0, -r}, {r,r}, {-r, r}, {r, -r}, {-r, -r}};
 	std::vector<std::pair<int, int>> output = {};
 	for (auto o : offsets) {
-		std::pair<int, int> new_point  = {xy[0] + o[0], xy[1] + o[1]};
+		std::pair<int, int> new_point  = {xy.first + o.first, xy.second + o.second};
 		if (is_state_free(new_point, input)) {
 			output.push_back(new_point);
 		}
@@ -64,7 +64,7 @@ std::vector<std::pair<int, int>> get_neighbors(AStar::Request input, int r, std:
 }
 
 double distance(std::pair<int, int> x1, std::pair<int, int> x2) {
-	return (x1[0] - x2[0]) * (x1[0] - x2[0]) + (x1[1] - x2[1]) * (x1[1] - x2[1])
+	return (x1.first - x2.first) * (x1.first - x2.first) + (x1.second - x2.second) * (x1.second - x2.second)
 }
 
 std::pair<int, int> find_best_est_cost_through(AStarAlgorithmParams params) {
@@ -80,9 +80,9 @@ std::pair<int, int> find_best_est_cost_through(AStarAlgorithmParams params) {
 }
 
 void reconstruct_path(AStarAlgorithmParams &params, AStar::Request req) {
-	params.path = {{req.x_goal[0], req.x_goal[1]}}; // reset the path
-	auto current_point = params.path[0];
-	while (current_point != {req.x_init[0], req.x_init[1]}) {
+	params.path = {{req.x_goal.first, req.x_goal.second}}; // reset the path
+	auto current_point = params.path.first;
+	while (current_point != {req.x_init.first, req.x_init.second}) {
 		params.path.push_back(params.came_from[current_point]);
 		current_point = params.came_from[current_point];
 	}
@@ -104,8 +104,8 @@ nav_msgs::AStarOutput create_path(AStarAlgorithmParams &params) {
 
 bool astar_path_plan(AStar::Request &req, AStar::Response &res) {
 	AStarAlgorithmParams algo_params;
-	std::pair<int, int> x_init = {req.x_init[0], req.x_init[1]};
-	std::pair<int, int> x_goal = {req.x_goal[0], req.x_goal[1]};
+	std::pair<int, int> x_init = {req.x_init.first, req.x_init.second};
+	std::pair<int, int> x_goal = {req.x_goal.first, req.x_goal.second};
 
 	algo_params.open_set.add(x_init);
 	algo_params.cost_to_arrive[x_init] = 0;
