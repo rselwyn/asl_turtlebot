971bdcc99028644b38dda4f79f0e96ba223bdb71
Devin Ardeshna <devin@ardeshna.com>
2021-12-08 02:13:47 -0800

    shared parameters

--- scripts/nav_params.py
+++ scripts/nav_params.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 import rospy
 from dynamic_reconfigure.client import Client
+from dynamic_reconfigure.server import Server
 from asl_turtlebot.cfg import NavigatorConfig
-
+import copy
 
 """ NAVIGATION CONSTANTS """
 
@@ -11,29 +14,40 @@ THETA_EPS = 0.05
 
 """ SHARED NAVIGATION PARAMETERS """
 
-_defs = [
-    "localizer", # "odom" or "map"
-    "v_max", # maximum velocity
-    "om_max", # maximum angular velocity
-]
-
 class _NavParams():
 
     def __init__(self):
+        self._client = None
+        self._params = None
+
+    def _initialize(self):
         self._params = {}
-        self._client = Client(NavigatorConfig, self._callback)
+        self._client = Client("nav_params", config_callback=self._callback)
+        self._params = copy.deepcopy(self._client.get_configuration())
 
     def _callback(self, configs):
-        self._params.update({k:v for k,v in zip(self._params.keys(), configs)})
+        self._params.update(configs)
 
     def _update(self, p, v):
-        self._client().
+        if not self._client:
+            self._initialize()
+        self._client.update_configuration({p:v})
+        self._params.update({p:v})
+
+    def _get(self, p):
+        if not self._client:
+            self._initialize()
+        return self._params[p]
 
     @classmethod
     def dot(cls, params):
         for p in params:
-            setattr(cls, p, property(fget=lambda s,p=p:s._params[p], fset=lambda s,v,p=p:rospy.set_param(p, v)))
-        return _NavParams()
+            setattr(cls, p, property(fget=lambda s,p=p:s._get(p), fset=lambda s,v,p=p:s._update(p, v)))
 
-nav_params = _NavParams()
-_NavParams.dot(nav_params._params.keys())
+if __name__ == "__main__":
+    rospy.init_node("nav_params", anonymous = False)
+    srv = Server(NavigatorConfig, lambda x,y:x)
+    rospy.spin()
+else:
+    nav_params = _NavParams()
+    _NavParams.dot([x['name'] for x in NavigatorConfig.config_description['parameters']])
